75b88cd6cdaefd3082ca845057f640a7ac83dd98
Xee308 <30619887+Xee308@users.noreply.github.com>
2020-11-12 00:12:19 +0200

    First Cmomit
    Adding files

---
+++ LwandaWebsite/assets/js/main.js
@@ -0,0 +1,7 @@
+
+document.getElementById("toggle-button").addEventListener("click", toggleFunction);
+
+function toggleFunction() {
+
+    document.getElementById("header-area").classList.toggle('nav-collapse');
+}
